8fc12af90b87a20b4b9a2cf4e9af6867a167c82c
samkzy <skho0041@student.monash.edu>
2024-09-24 21:29:13 +0800

    Created choropleth map Includes html page

---
+++ Visualisations/choropleth_map.js
@@ -0,0 +1,8 @@
+var vg_1 = "choropleth_map.vg.json";
+
+vegaEmbed("#choro_map", vg_1).then(function (result) {
+    
+    // Access the Vega view instance
+    // Documentation: https://vega.github.io/vega/docs/api/view/
+    var view = result.view;
+}).catch(console.error);
